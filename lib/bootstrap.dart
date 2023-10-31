edb605cda4c61db5052d81708b8829962aeaf137
iLukas <luislukas2222@gmail.com>
2023-10-31 17:42:39 -0500

    Initialize firebase

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -2,11 +2,14 @@ import 'dart:async';
 import 'dart:developer';
 
 import 'package:bloc/bloc.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:injectable/injectable.dart';
 import 'package:you_might_need_work/features/app/app_bloc_observer.dart';
+import 'package:you_might_need_work/firebase_options.dart';
 import 'package:you_might_need_work/injection.dart';
 
 Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
@@ -18,6 +21,10 @@ Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
     () async {
 
       WidgetsFlutterBinding.ensureInitialized();
+
+      await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
+      );
 
       await dotenv.load();
 
