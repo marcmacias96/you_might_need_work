f0f12668f53c01109ce7798685b46a5dc247cc8d
Marco Macias <marc.macias96@hotmail.com>
2023-10-31 13:30:24 -0500

    Config injectable, bloc and reactive forms generator

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -1,7 +1,13 @@
 import 'dart:async';
 import 'dart:developer';
 
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:injectable/injectable.dart';
+import 'package:you_might_need_work/features/app/app_bloc_observer.dart';
+import 'package:you_might_need_work/injection.dart';
 
 Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
   FlutterError.onError = (details) {
@@ -12,6 +18,19 @@ Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
     () async {
 
       WidgetsFlutterBinding.ensureInitialized();
+
+      await dotenv.load();
+
+       Bloc.observer = AppBlocObserver();
+
+      configureDependencies(Environment.prod);
+
+      await Future.wait([
+        SystemChrome.setPreferredOrientations([
+          DeviceOrientation.portraitUp,
+          DeviceOrientation.portraitDown,
+        ]),
+      ]);
 
       return runApp(await builder());
     },
