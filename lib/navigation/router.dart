d23b22965af5ee1c8917149ef1c9550b98bfdcfc
iLukas <luislukas2222@gmail.com>
2023-11-02 08:27:35 -0500

    Add OTP Form

--- lib/navigation/router.dart
+++ lib/navigation/router.dart
@@ -4,6 +4,7 @@ import 'package:you_might_need_work/features/auth/auth.dart';
 import 'package:you_might_need_work/features/auth_form/auth_form.dart';
 import 'package:you_might_need_work/features/home/home.dart';
 import 'package:you_might_need_work/features/onboarding/onboarding.dart';
+import 'package:you_might_need_work/features/otp_form/otp_form.dart';
 import 'package:you_might_need_work/features/profile_form/profile_form.dart';
 import 'package:you_might_need_work/utils/utils.dart';
 
@@ -45,6 +46,17 @@ GoRouter getGoRouter(AuthCubit authCubit) {
         path: '/${HomePage.routeName}',
         name: HomePage.routeName,
         builder: (context, state) => const HomePage(),
+      ),
+      GoRoute(
+        path: '/${OtpFormPage.routeName}',
+        name: OtpFormPage.routeName,
+        builder: (context, state) {
+          var args = const OtpFormArgs();
+          if (state.extra != null) args = state.extra! as OtpFormArgs;
+          return OtpFormPage(
+            args: args,
+          );
+        },
       ),
     ],
     redirect: (context, state) {
