70bbc0c42e4350748e09cbb1752f4957d536bdc2
iLukas <luislukas2222@gmail.com>
2023-11-02 07:46:37 -0500

    Add function go to Sign Up

--- lib/features/auth_form/auth_form_page.dart
+++ lib/features/auth_form/auth_form_page.dart
@@ -1,6 +1,7 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 import 'package:reactive_forms/reactive_forms.dart';
 import 'package:you_might_need_work/assets/assets.dart';
 import 'package:you_might_need_work/features/auth_form/cubit/cubit.dart';
@@ -41,7 +42,7 @@ class _AuthFormPageState extends State<AuthFormPage> {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     return GestureDetector(
-      onTap: (){
+      onTap: () {
         FocusManager.instance.primaryFocus?.unfocus();
       },
       child: BlocProvider(
@@ -55,102 +56,171 @@ class _AuthFormPageState extends State<AuthFormPage> {
               );
             },
             builder: (context, state) {
-              return  CustomScrollView(
-                  slivers: [
-                    SliverAppBar.medium(
-                      automaticallyImplyLeading: false,
-                      elevation: 0,
-                      centerTitle: true,
-                      title: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                              Text(
-                                widget.args.type == AuthFormType.login
-                                    ? 'Hello!'
-                                    : 'Sign Up',
-                                    textAlign: TextAlign.center,
-                                style: theme.textTheme.titleLarge,
-                              ),
-                            ],
-                      ),
+              return CustomScrollView(
+                slivers: [
+                  SliverAppBar.medium(
+                    automaticallyImplyLeading: false,
+                    elevation: 0,
+                    centerTitle: true,
+                    title: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          widget.args.type == AuthFormType.login
+                              ? 'Sign In'
+                              : 'Sign Up',
+                          textAlign: TextAlign.center,
+                          style: theme.textTheme.titleLarge,
+                        ),
+                      ],
                     ),
-                    SliverToBoxAdapter(
-                      child: CredentialsFormBuilder(
-                        model: credentials,
-                        builder: (context, formModel, _) {
-                          return Padding(
-                                padding: const EdgeInsets.symmetric
-                                (horizontal: AppPadding.large),
-                            child: Form(
-                              autovalidateMode:
+                  ),
+                  SliverToBoxAdapter(
+                    child: CredentialsFormBuilder(
+                      model: credentials,
+                      builder: (context, formModel, _) {
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: AppPadding.large,
+                          ),
+                          child: Form(
+                            autovalidateMode:
                                 AutovalidateMode.onUserInteraction,
-                              child: Column(
-                                children: [
-                                  AppFormInput(
-                                    prefixIcon: Images.email,
-                                    formControl: formModel.emailControl,
-                                    labelText: 'E-mail',
-                                    hintText: 'Enter your email',
-                                    validationMessages: {
-                                      ValidationMessage.required: (_) =>
-                                          'The email must not be empty',
-                                      ValidationMessage.email: (_) =>
-                                          'The email must be valid',
-                                    },
-                                  ),
-                                  const SizedBox(height: AppPadding.big),
-                                  AppFormInput(
-                                    prefixIcon: Images.lock,
-                                    suffixIcon: Images.eye,
-                                    formControl: formModel.passwordControl,
-                                    obscureText: true,
-                                    validationMessages: {
-                                      ValidationMessage.required: (_) =>
-                                          'The password must not be empty',
-                                      ValidationMessage.minLength: (_) =>
-                                          'The password must be'
-                                          ' at least 6 characters',
-                                    },
-                                    textInputAction: TextInputAction.done,
-                                    hintText: 'Password',
-                                    labelText: 'Password',
-                                  ),
-                                  const SizedBox(height: AppPadding.large),
-                                  Row(
-                                    mainAxisAlignment: MainAxisAlignment.end,
-                                    children: [
-                                      Text('Forgot Password?',style: 
-                                      theme.textTheme.bodyMedium!.copyWith(
+                            child: Column(
+                              children: [
+                                AppFormInput(
+                                  prefixIcon: Images.email,
+                                  formControl: formModel.emailControl,
+                                  labelText: 'E-mail',
+                                  hintText: 'Enter your email',
+                                  validationMessages: {
+                                    ValidationMessage.required: (_) =>
+                                        'The email must not be empty',
+                                    ValidationMessage.email: (_) =>
+                                        'The email must be valid',
+                                  },
+                                ),
+                                const SizedBox(height: AppPadding.big),
+                                AppFormInput(
+                                  prefixIcon: Images.lock,
+                                  suffixIcon: Images.eye,
+                                  formControl: formModel.passwordControl,
+                                  obscureText: true,
+                                  validationMessages: {
+                                    ValidationMessage.required: (_) =>
+                                        'The password must not be empty',
+                                    ValidationMessage.minLength: (_) =>
+                                        'The password must be'
+                                        ' at least 6 characters',
+                                  },
+                                  textInputAction: TextInputAction.done,
+                                  hintText: 'Password',
+                                  labelText: 'Password',
+                                ),
+                                const SizedBox(height: AppPadding.large),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    Text(
+                                      'Forgot Password?',
+                                      style:
+                                          theme.textTheme.bodyMedium!.copyWith(
                                         color: AppColors.grayGray1,
-                                      ),),
-                                    ],
-                                  ),
-                                  const SizedBox(height: AppPadding.xxxl),
-                                  ReactiveCredentialsFormConsumer(
-                                    builder: (context, form, child) {
-                                      return AppElevatedButton(
-                                        loading: state.isSubmitting,
-                                        onPressed: form.form.valid
-                                            ? () => handleAction(
-                                                  context,
-                                                  credentials: formModel.model,
-                                                )
-                                            : null,
-                                        text: widget.args.type
-                                         == AuthFormType.login
-                                            ? 'Sign In'
-                                            : 'Sign Up',
-                                      );
-                                    },
-                                  ),
-                                ],
-                              ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                const SizedBox(height: AppPadding.xxxl),
+                                ReactiveCredentialsFormConsumer(
+                                  builder: (context, form, child) {
+                                    return AppElevatedButton(
+                                      loading: state.isSubmitting,
+                                      onPressed: form.form.valid
+                                          ? () => handleAction(
+                                                context,
+                                                credentials: formModel.model,
+                                              )
+                                          : null,
+                                      text:
+                                          widget.args.type == AuthFormType.login
+                                              ? 'Sign In'
+                                              : 'Sign Up',
+                                    );
+                                  },
+                                ),
+                                const SizedBox(height: AppPadding.xxxl),
+                                GestureDetector(
+                                  onTap: widget.args.type == AuthFormType.login
+                                      ? () {
+                                          context.pushNamed(
+                                            AuthFormPage.routeName,
+                                            extra: const AuthFormArgs(
+                                              type: AuthFormType.signUp,
+                                            ),
+                                          );
+                                        }
+                                      : () {
+                                          context.pushNamed(
+                                            AuthFormPage.routeName,
+                                            extra: const AuthFormArgs(
+                                              type: AuthFormType.login,
+                                            ),
+                                          );
+                                        },
+                                  child: widget.args.type == AuthFormType.login
+                                      ? Text.rich(
+                                          TextSpan(
+                                            children: [
+                                              TextSpan(
+                                                text: 'Don’t Have Any Account?',
+                                                style: theme
+                                                    .textTheme.bodyMedium!
+                                                    .copyWith(
+                                                  color: AppColors.grayGray2,
+                                                ),
+                                              ),
+                                              TextSpan(
+                                                text: '  Sign Up',
+                                                style: theme
+                                                    .textTheme.bodyMedium!
+                                                    .copyWith(
+                                                  fontWeight: FontWeight.bold,
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        )
+                                      : Text.rich(
+                                          TextSpan(
+                                            children: [
+                                              TextSpan(
+                                                text: 'You have an account?',
+                                                style: theme
+                                                    .textTheme.bodyMedium!
+                                                    .copyWith(
+                                                  color: AppColors.grayGray2,
+                                                ),
+                                              ),
+                                              TextSpan(
+                                                text: '  Sign In',
+                                                style: theme
+                                                    .textTheme.bodyMedium!
+                                                    .copyWith(
+                                                  fontWeight: FontWeight.bold,
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                ),
+                              ],
                             ),
-                          );
-                        },
-                      ),
+                          ),
+                        );
+                      },
                     ),
-                  ],
+                  ),
+                ],
               );
             },
           ),
