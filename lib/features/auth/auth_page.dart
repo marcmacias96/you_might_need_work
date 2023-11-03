445282256127da276b759ba240c56a9a6ec0fbc7
Marco Macias <marc.macias96@hotmail.com>
2023-11-03 07:53:40 -0500

    Fix redirection

--- lib/features/auth/auth_page.dart
+++ lib/features/auth/auth_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 import 'package:you_might_need_work/features/auth/auth.dart';
+import 'package:you_might_need_work/features/profile_form/profile_form_page.dart';
 
 class AuthPage extends StatefulWidget {
   const AuthPage({super.key});
@@ -12,16 +14,27 @@ class AuthPage extends StatefulWidget {
 }
 
 class _AuthPageState extends State<AuthPage> {
-   @override
+  @override
   void initState() {
     context.read<AuthCubit>().authCheck();
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: CircularProgressIndicator.adaptive(),
+    return BlocListener<AuthCubit, AuthState>(
+      listener: (context, state) {
+        state.maybeMap(
+          orElse: () => null,
+          authenticated: (_) {
+            context.goNamed(ProfileFormPage.routeName);
+          },
+        );
+      },
+      child: const Scaffold(
+        body: Center(
+          child: CircularProgressIndicator.adaptive(),
+        ),
       ),
     );
   }
